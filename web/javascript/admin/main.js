e1c1079be8d71717f8dcbd11d92e394d71f8b830
System Administrator <deschamps.manon.91@gmail.com>
2017-03-02 17:26:20 +0100

    add datatable library

--- web/javascript/admin/main.js
+++ web/javascript/admin/main.js
@@ -2,12 +2,22 @@ function page_width() {
 	var main_header_width = $("#main-header").width();
 	var window_width = $(window).width();
 	var calc_width = window_width - main_header_width;
-	//alert(calc_width)
 	$(".page").css("width",calc_width);
 }
 
+function display_user_roles(){
+	$(".user-roles").each(function(){
+		var content = $(this).text();
+		//content = $.trim(content);
+		var new_content = content.replace(/_/g, ' ').replace(/role/gi, '');;
+		console.log(new_content)
+		$(this).text(new_content)
+	})
+}
+
 $(document).ready(function(){
-	page_width()
+	page_width();
+	display_user_roles();
 })
 
 $(window).resize(function(){
---
+++ web/javascript/library/datatable-launcher.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('.datatable').DataTable({
+		"language": {
+            //processing:     "I18n.datatable_processin"g,
+	        search:         "",
+	        //lengthMenu:     I18n.datatable_lengthMenu,
+	        //info:           I18n.datatable_info,
+	        //infoEmpty:      I18n.datatable_infoEmpty,
+	        //infoFiltered:   I18n.datatable_infoFiltered,
+	        //infoPostFix:    "",
+	        //loadingRecords: I18n.datatable_loadingRecords,
+	        //zeroRecords:    I18n.datatable_zeroRecords,
+	        //emptyTable:     I18n.datatable_emptyTable,
+	        paginate: {
+	          //first:      I18n.datatable_paginate_first,
+	          //last:       I18n.datatable_paginate_last
+	        }
+		}
+	});
+})
