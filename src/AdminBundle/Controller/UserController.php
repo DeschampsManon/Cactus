c12759d485861b4547c9e27555e0e4e479ce4306
Manon <manon.deschamps@plezi.co>
2017-03-10 12:02:03 +0100

    add action update user

--- src/AdminBundle/Controller/UserController.php
+++ src/AdminBundle/Controller/UserController.php
@@ -15,25 +15,26 @@ class UserController extends Controller {
         return $this->render('AdminBundle:Users:index.html.twig', array('users' =>  $users));
     }
 
-    public function updateAction($id, Request $request)
-	{	
-		$userManager = $this->get('fos_user.user_manager');
+    public function updateAction($id,Request $request)
+    {
+        $userManager = $this->get('fos_user.user_manager');
         $user = $userManager->findUserBy(array('id' => $id));
 
         $form = $this->createForm(UserType::class, $user);
-		//$request = $this->get('request');
-    	
-    	//if ($request->getMethod() == 'POST') {
-			if ($form->isValid()) {
-				$userManager = $this->container->get('fos_user.user_manager');
-				$userManager->updateUser($user);
-				return $this->redirectToRoute('admin_show_users');
-			}
-		//}
 
+        $form->handleRequest($request);
 
-	    return $this->render('AdminBundle:Users:edit.html.twig', array('user' =>  $user,'form' => $form->createView()));
-	}
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userManager = $this->container->get('fos_user.user_manager');
+            $userManager->updateUser($user);
+            return $this->redirectToRoute('admin_show_users');
+        }
+
+        return $this->render(
+            'AdminBundle:Users:edit.html.twig', 
+            array('form' => $form->createView(), 'user' =>  $user)
+        );
+    }
 
 	public function createAction() {
         
